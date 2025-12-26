3a7dcc4fbbf1db88a129d3397312188f94715492
vynhv <vy.nhv2004@gmail.com>
2025-12-26 15:38:22 +0700

    Add end-to-end test for private chat using dummy clients (CHAT-015)

--- src/main/java/org/example/VChatTalk/handler/ChatWebSocketHandler.java
+++ src/main/java/org/example/VChatTalk/handler/ChatWebSocketHandler.java
@@ -235,7 +235,6 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
             switch (result.getType()) {
 
                 case NONE -> {
-                    // do nothing
                 }
                 case HELP ->{
                     String commands = "/help - Show available commands\n"
